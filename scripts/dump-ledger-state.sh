40da8148d85f8b07c60fcce6de68b4443aef0bed
Ryan Williams <ryan.williams@intersectmbo.org>
2024-01-28 00:01:05 +0000

    add new dump scripts

--- scripts/dump-ledger-state.sh
+++ scripts/dump-ledger-state.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Dumping ledger state
-echo "Dumping governance ledger state."
+echo "Dumping whole ledger state."
 
 # Set alias for convenience
 alias sancho-cli="docker exec -ti sancho-node cardano-cli"
 
-sancho-cli conway query gov-state --testnet-magic 4 > ./dumps/gov-state.json
+sancho-cli conway query ledger-state --testnet-magic 4 > ./dumps/ledger-state.json
--- scripts/dump-protocol-state.sh
+++ scripts/dump-protocol-state.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Dumping ledger state
-echo "Dumping governance ledger state."
+echo "Dumping protocol state."
 
 # Set alias for convenience
 alias sancho-cli="docker exec -ti sancho-node cardano-cli"
 
-sancho-cli conway query gov-state --testnet-magic 4 > ./dumps/gov-state.json
+sancho-cli conway query protocol-state --testnet-magic 4 > ./dumps/protocol-state.json

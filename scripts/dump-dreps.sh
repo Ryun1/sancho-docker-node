d1c7f7b83e156e164c3652aa72c599b7eb274f36
Ryan Williams <ryan.williams@intersectmbo.org>
2024-02-24 23:05:30 +0000

    fix DRep dumps

--- scripts/dump-dreps.sh
+++ scripts/dump-dreps.sh
@@ -6,6 +6,10 @@ echo "Querying DReps from ledger state."
 # Set alias for convenience
 alias sancho-cli="docker exec -ti sancho-node cardano-cli"
 
-sancho-cli conway query drep-state --testnet-magic 4 > ./dumps/dreps-info.json
+sancho-cli conway query drep-state \
+    --testnet-magic 4 \
+    --all-dreps > ./dumps/dreps-info.json
 
-sancho-cli conway query drep-stake-distribution --testnet-magic 4 > ./dumps/dreps-power.json
+sancho-cli conway query drep-stake-distribution \
+    --testnet-magic 4 \
+    --all-dreps > ./dumps/dreps-power.json

330d5aae68d45a97c4e9563938757b98497b2124
Ryan Williams <ryan.williams@intersectmbo.org>
2024-03-14 15:46:39 -0100

    tweak cc scripts

--- scripts/cc/cc-generate-keys.sh
+++ scripts/cc/cc-generate-keys.sh
@@ -15,18 +15,18 @@ alias container-cli="docker exec -ti sancho-node cardano-cli"
 
 # Generate CC cold keys
 container-cli conway governance committee key-gen-cold \
- --verification-key-file keys/cc-cold.vkey \
- --signing-key-file keys/cc-cold.skey \
+ --verification-key-file ./keys/cc-cold.vkey \
+ --signing-key-file ./keys/cc-cold.skey \
 
 # Generate CC hot keys
 container-cli conway governance committee key-gen-hot \
- --verification-key-file keys/cc-hot.vkey \
- --signing-key-file keys/cc-hot.skey \
+ --verification-key-file ./keys/cc-hot.vkey \
+ --signing-key-file ./keys/cc-hot.skey \
 
-# Generate CC cold key hash
+# Generate CC cold key hash, just so we have it
 container-cli conway governance committee key-hash \
- --verification-key-file keys/cc-cold.vkey > keys/cc-cold-key-hash.hash
+ --verification-key-file ./keys/cc-cold.vkey > keys/cc-cold-key-hash.hash
 
-# Generate CC hot key hash
+# Generate CC cold key hash, just so we have it
 container-cli conway governance committee key-hash \
- --verification-key-file keys/cc-hot.vkey > keys/cc-hot-key-hash.hash
+ --verification-key-file ./keys/cc-hot.vkey > keys/cc-hot-key-hash.hash

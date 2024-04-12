deef886b5ee3477e7d58d8e898183ae3f58b2171
Ryan Williams <ryan.williams@intersectmbo.org>
2024-04-12 16:40:48 -0600

    bump to node 8.10.0-pre

--- scripts/stake/delegate-to-spo.sh
+++ scripts/stake/delegate-to-spo.sh
@@ -10,4 +10,26 @@ echo "Delegating you to SPO: $spo_id."
 # Set alias for convenience
 alias container-cli="docker exec -ti sancho-node cardano-cli"
 
-# TODO
+container-cli conway stake-address stake-delegation-certificate \
+ --stake-verification-key-file ./keys/stake.vkey \
+ --stake-pool-id "$spo_id" \
+ --out-file ./txs/stake-deleg-key-hash.cert
+
+container-cli conway transaction build \
+ --testnet-magic 4 \
+ --witness-override 2 \
+ --tx-in $(container-cli query utxo --address $(cat ./keys/payment.addr) --testnet-magic 4 --out-file  /dev/stdout | jq -r 'keys[0]') \
+ --change-address $(cat ./keys/payment.addr) \
+ --certificate-file ./txs/stake-deleg-key-hash.cert \
+ --out-file ./txs/stake-deleg-tx.raw
+
+container-cli conway transaction sign \
+ --tx-body-file ./txs/stake-deleg-tx.raw \
+ --signing-key-file ./keys/payment.skey \
+ --signing-key-file ./keys/stake.skey \
+ --testnet-magic 4 \
+ --out-file ./txs/stake-deleg-tx.signed
+
+container-cli conway transaction submit \
+ --testnet-magic 4 \
+ --tx-file ./txs/stake-deleg-tx.signed

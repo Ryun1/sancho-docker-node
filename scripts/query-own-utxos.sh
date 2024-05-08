8044aa309d282ddd775c829ed09f6c6f74f2c7c7
Ryan Williams <ryan.williams@intersectmbo.org>
2024-05-08 13:51:01 +0100

    general clean up

--- scripts/query-own-utxos.sh
+++ scripts/query-own-utxos.sh
@@ -2,9 +2,9 @@
 
 # Check if you have a address created
 if [ ! -f "./keys/payment.addr" ]; then
-    echo "Please generate some keys and addresses before querying funds."
-    echo "Exiting."
-    exit 0
+  echo "Please generate some keys and addresses before querying funds."
+  echo "Exiting."
+  exit 0
 fi
 
 # Function to execute cardano-cli commands inside the container
@@ -16,5 +16,6 @@ echo "Querying UTXOs for your address: $(cat ./keys/payment.addr)"
 
 # Query the UTxOs controlled by the payment address
 container_cli query utxo \
-    --address $(cat ./keys/payment.addr) \
-    --testnet-magic 4 --out-file  /dev/stdout
+  --address "$(cat ./keys/payment.addr)" \
+  --testnet-magic 4 \
+  --out-file  /dev/stdout

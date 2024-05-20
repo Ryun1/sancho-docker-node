683a7935cff0e525f49c65c9a4ea12465ba7a308
Ryan Williams <ryan.williams@intersectmbo.org>
2024-05-20 13:18:20 +0100

    add orchestrator minting

--- scripts/cc-manager/orchestrator/README.md
+++ scripts/cc-manager/orchestrator/README.md
@@ -1,4 +1,21 @@
 
-## Setup NFTs
+## Orchestrator Flows
 
-1. 
+1. `./generate-orchestrator-credentials.sh`
+
+Generates payment, stake keys for orchestrator and payment address.
+Has a check to not overwrite existing keys.
+
+**in real world:** Orchestrator will already have a set of credentials on their own setup.
+
+2. `./mint-sign-submit-nfts.sh`
+
+Using the cold and hot minting policies to mint one cold and one hot NFT, sending to orchestrator address.
+This transaction needs to be signed by the keys included within the minting policy scripts, one for the cold and one for the hot.
+⚠️ Important to double check that YOU control the orchestrator address.
+
+**in real world:** Ran by the orchestrator on their own setup, or in conjunction with head of security.
+
+3. `./check-orchestrator-utxos.sh`
+
+This gives an output to console showing the UTxOs controlled by the orchestrator address
--- scripts/cc-manager/orchestrator/mint-sign-submit-nfts.sh
+++ scripts/cc-manager/orchestrator/mint-sign-submit-nfts.sh
@@ -10,7 +10,30 @@ orchestrator_script_path="./keys/cc-manager/orchestrator/nft"
 orchestrator_tx_path="./txs/orchestrator"
 nft_mint_key_path="./keys/nft-mint-keys"
 
+echo "\nBuilding transaction to mint hot and cold NFTs and send them to the orchestrator address"
+
+echo " "
+echo "Minting hot and cold NFTs and sending to orchestrator address."
+echo "Orchestraor address: $(cat $orchestrator_key_path/orchestrator.addr)"
+echo "Cold script policy: $(cat $orchestrator_script_path/native-cold-script.pol)"
+echo "Hot script policy: $(cat $orchestrator_script_path/native-hot-script.pol)"
+echo "Cold minting key hash: $(cat $nft_mint_key_path/cold.hash)"
+echo "Hot minting key hash: $(cat $nft_mint_key_path/hot.hash)"
+echo " "
+
+read -p "Are you happy with this info (yes/no): " continue
+
+if [ "$continue" = "yes" ]; then
+  continue
+else
+  echo "Aborting script."
+  exit 1
+fi
+
 # First we create the two NFTs, making sure that we use the correct minting keys as determined by the scripts
+
+echo "\nBuidling transacion"
+
 container_cli conway transaction build \
  --testnet-magic 4 \
  --tx-in $(container_cli query utxo --address $(cat $orchestrator_key_path/orchestrator.addr) --output-json --testnet-magic 4 | jq -r 'keys[0]') \
@@ -24,7 +47,10 @@ container_cli conway transaction build \
  --required-signer-hash "$(cat "${nft_mint_key_path}/hot.hash")" \
  --out-file $orchestrator_tx_path/mint-nfts.raw
 
-# Sign transcaction
+# Sign transaction
+
+echo "\nSigning transacion"
+
 container_cli transaction sign \
  --testnet-magic 4 \
  --signing-key-file $orchestrator_key_path/orchestrator.skey \
@@ -33,6 +59,24 @@ container_cli transaction sign \
  --tx-body-file $orchestrator_tx_path/mint-nfts.raw \
  --out-file $orchestrator_tx_path/mint-nfts.signed
 
-# container_cli transaction submit \
-#  --testnet-magic 4 \
-#  --tx-file $orchestrator_tx_path/mint-nfts.signed
+# Submit transaction
+
+echo "\nSubmitting transacion"
+
+echo " "
+echo "Orchestraor address: $(cat $orchestrator_key_path/orchestrator.addr)"
+echo "Cold script policy: $(cat $orchestrator_script_path/native-cold-script.pol)"
+echo "Hot script policy: $(cat $orchestrator_script_path/native-hot-script.pol)"
+echo "Cold minting key hash: $(cat $nft_mint_key_path/cold.hash)"
+echo "Hot minting key hash: $(cat $nft_mint_key_path/hot.hash)"
+echo " "
+read -p "Are you sure want to submit this transaction? (yes/no): " answer
+
+if [ "$answer" = "yes" ]; then
+  container_cli transaction submit \
+   --testnet-magic 4 \
+   --tx-file $orchestrator_tx_path/mint-nfts.signed
+else
+  echo "Transaction submission cancelled."
+  exit 1
+fi

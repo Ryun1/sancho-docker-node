1287763203204459c87a978091178f4c65db49d0
Ryan Williams <ryan.williams@intersectmbo.org>
2024-12-31 23:14:43 +0000

    finish cc script refactor

--- scripts/cc/generate-new-hot-key.sh
+++ scripts/cc/generate-new-hot-key.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-# Generate hot cc keys
-echo "Generating new consitutional committee hot key."
+# Define directories
+keys_dir="./keys"
 
 # Function to execute cardano-cli commands inside the container
 container_cli() {
   docker exec -ti sancho-node cardano-cli "$@"
 }
 
+# Generate hot CC keys
+echo "Generating a new constitutional committee hot key (replacing the existing one)."
+
 # Generate CC hot keys
 container_cli conway governance committee key-gen-hot \
- --verification-key-file keys/cc-hot.vkey \
- --signing-key-file keys/cc-hot.skey \
+  --verification-key-file "$keys_dir/cc-hot.vkey" \
+  --signing-key-file "$keys_dir/cc-hot.skey"
 
 # Generate CC hot key hash
 container_cli conway governance committee key-hash \
- --verification-key-file keys/cc-hot.vkey > keys/cc-hot-key-hash.hash
+  --verification-key-file "$keys_dir/cc-hot.vkey" > "$keys_dir/cc-hot-key-hash.hash"

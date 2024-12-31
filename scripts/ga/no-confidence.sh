9c8ead382f1cd2b351d6b1c09e25597bc6e31b74
Ryan Williams <ryan.williams@intersectmbo.org>
2024-12-31 23:43:29 +0000

    refactor ga scripts

--- scripts/ga/no-confidence.sh
+++ scripts/ga/no-confidence.sh
@@ -8,44 +8,46 @@ METADATA_URL="https://raw.githubusercontent.com/IntersectMBO/governance-actions/
 METADATA_HASH="ab901c3aeeca631ee5c70147a558fbf191a4af245d8ca001e845d8569d7c38f9"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Building, signing and submitting an no-confidence change governance action
-echo "Creating and submitting no-confidence governance action."
+# Define directories
+keys_dir="./keys"
+txs_dir="./$txs_dir/ga"
 
 # Function to execute cardano-cli commands inside the container
 container_cli() {
   docker exec -ti sancho-node cardano-cli "$@"
 }
 
+# Building, signing and submitting an no-confidence change governance action
+echo "Creating and submitting no-confidence governance action."
+
 container_cli conway governance action create-no-confidence \
   --testnet \
   --governance-action-deposit $(container_cli conway query gov-state --testnet-magic 4 | jq -r '.currentPParams.govActionDeposit') \
-  --deposit-return-stake-verification-key-file ./keys/stake.vkey \
+  --deposit-return-stake-verification-key-file ./$keys_dir/stake.vkey \
   --anchor-url "$METADATA_URL" \
   --anchor-data-hash "$METADATA_HASH" \
-  --out-file ./txs/no-confidence.action
+  --out-file ./$txs_dir/no-confidence.action
 
   # --prev-governance-action-tx-id "$PREV_GA_TX_HASH" \
   # --prev-governance-action-index "$PREV_GA_INDEX" \
 
-echo "Building the transaction."
-
 container_cli conway transaction build \
  --testnet-magic 4 \
- --tx-in "$(container_cli conway query utxo --address "$(cat ./keys/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[0]')" \
- --tx-in "$(container_cli conway query utxo --address "$(cat ./keys/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[1]')" \
- --tx-in "$(container_cli conway query utxo --address "$(cat ./keys/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[3]')" \
- --tx-in-collateral "$(container_cli conway query utxo --address "$(cat ./keys/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[1]')" \
- --proposal-file ./txs/no-confidence.action \
- --change-address "$(cat ./keys/payment.addr)" \
- --out-file ./txs/no-confidence-action-tx.unsigned
+ --tx-in "$(container_cli conway query utxo --address "$(cat ./$keys_dir/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[0]')" \
+ --tx-in "$(container_cli conway query utxo --address "$(cat ./$keys_dir/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[1]')" \
+ --tx-in "$(container_cli conway query utxo --address "$(cat ./$keys_dir/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[3]')" \
+ --tx-in-collateral "$(container_cli conway query utxo --address "$(cat ./$keys_dir/payment.addr)" --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[1]')" \
+ --proposal-file ./$txs_dir/no-confidence.action \
+ --change-address "$(cat ./$keys_dir/payment.addr)" \
+ --out-file ./$txs_dir/no-confidence-action-tx.unsigned
 
 container_cli conway transaction sign \
- --tx-body-file ./txs/no-confidence-action-tx.unsigned \
- --signing-key-file ./keys/payment.skey \
+ --tx-body-file ./$txs_dir/no-confidence-action-tx.unsigned \
+ --signing-key-file ./$keys_dir/payment.skey \
  --testnet-magic 4 \
- --out-file ./txs/no-confidence-action-tx.signed
+ --out-file ./$txs_dir/no-confidence-action-tx.signed
 
 container_cli conway transaction submit \
  --testnet-magic 4 \
- --tx-file ./txs/no-confidence-action-tx.signed
+ --tx-file ./$txs_dir/no-confidence-action-tx.signed
 

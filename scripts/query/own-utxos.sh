bbe407f4d203e14077439cb0a60f99548ae76a29
Ryan Williams <ryan.williams@intersectmbo.org>
2024-12-31 23:58:14 +0000

    stake scripts

--- scripts/query/own-utxos.sh
+++ scripts/query/own-utxos.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-# Check if you have a address created
-if [ ! -f "./keys/payment.addr" ]; then
-  echo "Please generate some keys and addresses before querying funds."
-  echo "Exiting."
-  exit 0
-fi
+# Define directories
+keys_dir="./keys"
 
 # Function to execute cardano-cli commands inside the container
 container_cli() {
   docker exec -ti sancho-node cardano-cli "$@"
 }
 
-echo "Querying UTXOs for your address: $(cat ./keys/payment.addr)"
+# Check if you have a address created
+if [ ! -f "./$keys_dir/payment.addr" ]; then
+  echo "Please generate some keys and addresses before querying funds."
+  echo "Exiting."
+  exit 0
+fi
+
+echo "Querying UTXOs for your address: $(cat ./$keys_dir/payment.addr)"
 
 # Query the UTxOs controlled by the payment address
 container_cli conway query utxo \
-  --address "$(cat ./keys/payment.addr)" \
+  --address "$(cat ./$keys_dir/payment.addr)" \
   --testnet-magic 4 \
   --out-file  /dev/stdout

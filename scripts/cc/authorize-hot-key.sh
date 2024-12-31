7ac1fa2ae6ec5134854a20469d34c017062f729d
Ryan Williams <ryan.williams@intersectmbo.org>
2024-12-31 23:13:47 +0000

    general tidy and refactor for cc scripts

--- scripts/cc/authorize-hot-key.sh
+++ scripts/cc/authorize-hot-key.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
 
+# Define directories
+keys_dir="./keys"
+txs_dir="./txs/cc"
+
 # Function to execute cardano-cli commands inside the container
 container_cli() {
   docker exec -ti sancho-node cardano-cli "$@"
 }
 
-hot_cred_file="./keys/cc-hot.vkey"
+echo "Authorizing hot key for the constitutional committee."
 
 container_cli conway governance committee create-hot-key-authorization-certificate \
-  --cold-verification-key-file ./keys/cc-cold.vkey \
-  --hot-key-file $hot_cred_file \
-  --out-file ./txs/cc-auth-hot.cert \
-
-# --hot-script-file $hot_cred_file \
+  --cold-verification-key-file "$keys_dir/cc-cold.vkey" \
+  --hot-key-file "$keys_dir/cc-hot.vkey" \
+  --out-file "$txs_dir/auth-hot.cert"
 
 container_cli conway transaction build \
   --testnet-magic 4 \
   --witness-override 2 \
-  --tx-in $(container_cli conway query utxo --address $(cat ./keys/payment.addr) --testnet-magic 4 --out-file  /dev/stdout | jq -r 'keys[0]') \
-  --change-address $(cat ./keys/payment.addr) \
-  --certificate-file ./txs/cc-auth-hot.cert \
-  --out-file ./txs/cc-auth-hot-tx.raw
+  --tx-in $(container_cli conway query utxo --address $(cat "$keys_dir/payment.addr") --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[0]') \
+  --change-address $(cat "$keys_dir/payment.addr") \
+  --certificate-file "$txs_dir/auth-hot.cert" \
+  --out-file "$txs_dir/auth-hot-tx.raw"
 
 container_cli conway transaction sign \
-  --tx-body-file ./txs/cc-auth-hot-tx.raw \
-  --signing-key-file ./keys/payment.skey \
-  --signing-key-file ./keys/cc-cold.skey \
+  --tx-body-file "$txs_dir/auth-hot-tx.raw" \
+  --signing-key-file "$keys_dir/payment.skey" \
+  --signing-key-file "$keys_dir/cc-cold.skey" \
   --testnet-magic 4 \
-  --out-file ./txs/cc-auth-hot-tx.signed
+  --out-file "$txs_dir/auth-hot-tx.signed"
 
 container_cli conway transaction submit \
   --testnet-magic 4 \
-  --tx-file ./txs/cc-auth-hot-tx.signed
-
+  --tx-file "$txs_dir/auth-hot-tx.signed"

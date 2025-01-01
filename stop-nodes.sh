8a200cb122931710bf5a8cb660676a9ad92b350b
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-01 02:42:35 +0000

    fix workflow

--- stop-nodes.sh
+++ stop-nodes.sh
@@ -3,3 +3,5 @@
 NETWORK=sanchonet envsubst < docker-compose.yml | docker-compose -f - down
 
 NETWORK=preview envsubst < docker-compose.yml | docker-compose -f - down
+
+NETWORK=preprod envsubst < docker-compose.yml | docker-compose -f - down

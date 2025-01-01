bb78adc5b65498a9fc642d53693183df4ff23640
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-01 01:19:27 +0000

    reasonable way to deal with multiple nodes

--- scripts/helper/check-running-containers.sh
+++ scripts/helper/check-running-containers.sh
@@ -10,14 +10,16 @@ IFS=$'\n' read -r -d '' -a running_containers <<< "$running_containers"
 if [ ${#running_containers[@]} -eq 1 ]; then
   echo "${running_containers[0]}"
 elif [ ${#running_containers[@]} -gt 1 ]; then
-  echo "Multiple running containers found. Please select one:"
   select container_name in "${running_containers[@]}"; do
     if [ -n "$container_name" ]; then
       echo "$container_name"
       break
+    else
+      echo "Invalid selection."
+      exit 1
     fi
   done
 else
-  echo "No running containers found from the predefined list."
+  echo "No running containers found."
   exit 1
 fi

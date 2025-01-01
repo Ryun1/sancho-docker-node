423cd52079ef02ad828ddb3b30103731ea3449f7
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-01 00:10:07 +0000

    spacing

--- scripts/query/dump-node-state.sh
+++ scripts/query/dump-node-state.sh
@@ -9,39 +9,39 @@ container_cli() {
 echo "Dumping DReps from ledger state."
 
 container_cli conway query drep-state \
-    --testnet-magic 4 \
-    --all-dreps > ./dumps/dreps-info.json
+  --testnet-magic 4 \
+  --all-dreps > ./dumps/dreps-info.json
 
 container_cli conway query drep-stake-distribution \
-    --testnet-magic 4 \
-    --all-dreps > ./dumps/dreps-power.json
+  --testnet-magic 4 \
+  --all-dreps > ./dumps/dreps-power.json
 
 # Dumping ledger state
 echo "Dumping governance ledger state."
 
 container_cli conway query gov-state \
-    --testnet-magic 4 > ./dumps/gov-state.json
+  --testnet-magic 4 > ./dumps/gov-state.json
 
 # Dumping ledger state
 echo "Dumping whole ledger state."
 
 container_cli conway query ledger-state \
-    --testnet-magic 4 > ./dumps/ledger-state.json
+  --testnet-magic 4 > ./dumps/ledger-state.json
 
 # Dumping proposals stored in ledger state
 echo "Dumping governance actions in ledger state."
 
 container_cli conway query gov-state \
-    --testnet-magic 4 | jq -r '.proposals' > ./dumps/governance-actions.json
+  --testnet-magic 4 | jq -r '.proposals' > ./dumps/governance-actions.json
 
 # Dumping out CC state
 echo "Dumping constitutional committee state."
 
 container_cli conway query committee-state \
-    --testnet-magic 4 > ./dumps/committee.json
+  --testnet-magic 4 > ./dumps/committee.json
 
 # Dumping out parameters state
 echo "Dumping protocol parameters state."
 
 container_cli conway query protocol-parameters \
-    --testnet-magic 4 > ./dumps/params.json
+  --testnet-magic 4 > ./dumps/params.json

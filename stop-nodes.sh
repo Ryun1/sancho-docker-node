497f8543d4559c5efd07a00cd9bc44b63f130690
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-01 23:17:59 +0000

    add mainnet to stop command

--- stop-nodes.sh
+++ stop-nodes.sh
@@ -5,3 +5,5 @@ NETWORK=sanchonet envsubst < docker-compose.yml | docker-compose -f - down
 NETWORK=preview envsubst < docker-compose.yml | docker-compose -f - down
 
 NETWORK=preprod envsubst < docker-compose.yml | docker-compose -f - down
+
+NETWORK=mainnet envsubst < docker-compose.yml | docker-compose -f - down

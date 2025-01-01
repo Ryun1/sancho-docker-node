a7dced4c7d6d38b37a2255c453e39c6c72cf3b51
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-01 00:39:27 +0000

    fix drep register

--- scripts/drep/register.sh
+++ scripts/drep/register.sh
@@ -1,4 +1,3 @@
-# filepath: /Users/ryan/cardano/node/sancho-docker-node/scripts/drep/register.sh
 #!/bin/sh
 
 # Define directories
@@ -19,8 +18,34 @@ fi
 echo "Using running container: $container_name"
 
 # Registering you as a drep
-echo "Registering you as a DRep"
+echo "Registering you as a DRep."
 
 container_cli conway governance drep registration-certificate \
  --drep-key-hash "$(cat $keys_dir/drep.id)" \
- --key-reg-deposit-amt "$(container_cli conway query gov-state --testnet-magic 4 | jq -r .currentPParams.dRepDeposit)"
+ --key-reg-deposit-amt "$(container_cli conway query gov-state --testnet-magic 4 | jq -r .currentPParams.dRepDeposit)" \
+ --out-file $txs_dir/drep-register.cert
+
+echo "Building transaction"
+
+container_cli conway transaction build \
+ --testnet-magic 4 \
+ --witness-override 2 \
+ --tx-in $(container_cli conway query utxo --address $(cat $keys_dir/payment.addr) --testnet-magic 4 --out-file  /dev/stdout | jq -r 'keys[0]') \
+ --change-address $(cat $keys_dir/payment.addr) \
+ --certificate-file $txs_dir/drep-register.cert \
+ --out-file $txs_dir/drep-reg-tx.unsigned
+
+echo "Signing transaction"
+
+container_cli conway transaction sign \
+ --tx-body-file $txs_dir/drep-reg-tx.unsigned \
+ --signing-key-file $keys_dir/payment.skey \
+ --signing-key-file $keys_dir/drep.skey \
+ --testnet-magic 4 \
+ --out-file $txs_dir/drep-reg-tx.signed
+
+echo "Submitting transaction"
+
+container_cli conway transaction submit \
+ --testnet-magic 4 \
+ --tx-file $txs_dir/drep-reg-tx.signed

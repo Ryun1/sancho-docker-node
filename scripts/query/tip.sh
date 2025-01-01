2c672d55a9c54d496f6567fec30f34351a77a289
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-01 01:19:27 +0000

    reasonable way to deal with multiple nodes

--- scripts/query/tip.sh
+++ scripts/query/tip.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+# Get the script's directory
+script_dir=$(dirname "$0")
+
+# Get the container name from the check-running-containers script
+container_name="$("$script_dir/../helper/check-running-containers.sh")"
+
+if [ -z "$container_name" ]; then
+  echo "Failed to determine a running container."
+  exit 1
+fi
+
+echo "Using running container: $container_name"
+
 # Function to execute cardano-cli commands inside the container
 container_cli() {
-  docker exec -ti sancho-node cardano-cli "$@"
+  docker exec -ti $container_name cardano-cli "$@"
 }
 
 # Query the tip of the blockchain as observed by the node

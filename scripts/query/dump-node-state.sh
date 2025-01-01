f4fc6945606c9131ff11bbc4a76c824dbc1e9dd5
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-01 23:05:36 +0000

    refactor and improve dumping node state logic

--- scripts/query/dump-node-state.sh
+++ scripts/query/dump-node-state.sh
@@ -18,35 +18,52 @@ container_cli() {
   docker exec -ti $container_name cardano-cli "$@"
 }
 
-# Query DReps from ledger state
-echo "Dumping DReps from ledger state."
+# Get the network name
 
-container_cli conway query drep-state > ./dumps/dreps-info.json
-
-container_cli conway query drep-stake-distribution > ./dumps/dreps-power.json
-
-# Dumping ledger state
-echo "Dumping governance ledger state."
-
-container_cli conway query gov-state > ./dumps/gov-state.json
-
-# Dumping ledger state
-echo "Dumping whole ledger state."
-
-container_cli conway query ledger-state > ./dumps/ledger-state.json
-
-# Dumping proposals stored in ledger state
-echo "Dumping governance actions in ledger state."
-
-container_cli conway query gov-state \
-  | jq -r '.proposals' > ./dumps/governance-actions.json
+# Split the container name and extract the second part
+network=$(echo $container_name | cut -d'-' -f2)
 
 # Dumping out CC state
 echo "Dumping constitutional committee state."
 
-container_cli conway query committee-state > ./dumps/committee.json
+container_cli conway query committee-state > ./dumps/$network/committee.json
+
+# Dumping out constitution state
+echo "Dumping constitution state."
+
+container_cli conway query  constitution > ./dumps/$network/constitution.json
+
+# Query DReps from ledger state
+echo "Dumping DReps from ledger state."
+
+container_cli conway query drep-state \
+  --all-dreps > ./dumps/$network/dreps-info.json
+
+container_cli conway query drep-stake-distribution \
+  --all-dreps > ./dumps/$network/dreps-power.json
+
+# Dumping governance ledger state
+echo "Dumping whole governance state from ledger state."
+
+container_cli conway query gov-state > ./dumps/$network/gov-state.json
+
+# Dumping proposals stored in ledger state
+echo "Dumping governance actions from ledger state."
+
+container_cli conway query gov-state | jq -r '.proposals' > ./dumps/$network/governance-actions.json
 
 # Dumping out parameters state
 echo "Dumping protocol parameters state."
 
-container_cli conway query protocol-parameters > ./dumps/params.json
+container_cli conway query protocol-parameters > ./dumps/$network/params.json
+
+# Dumping out SPO state
+echo "Dumping SPO stake distribution state."
+
+container_cli conway query spo-stake-distribution \
+ --all-spos > ./dumps/$network/spo-stake.json
+
+# Dumping out SPO state
+echo "Dumping treasury state."
+
+container_cli conway query treasury > ./dumps/$network/treasury.json
